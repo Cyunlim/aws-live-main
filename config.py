2aa03c78bf9c82715761d9ad88867a535b50ff22
Cyunlim <126947298+Cyunlim@users.noreply.github.com>
2025-12-02 13:25:29 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "employee.cf8s2wkwcd3x.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
-custompass = "Bait3273"
+custompass = "bait3273"
 customdb = "employee"
 custombucket = "chaiyun-s3-bucket"
 customregion = "us-east-1"
